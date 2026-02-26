359a661b52c1e9db357e8270f183c9e9aec9c335
ThirdPrince <18796017665@163.com>
2026-02-26 12:31:56 +0800

    - Set Java source and target compatibility to version 17 in `domain/build.gradle.kts`.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
     // Coroutines
     implementation(libs.kotlinx.coroutines.core)
