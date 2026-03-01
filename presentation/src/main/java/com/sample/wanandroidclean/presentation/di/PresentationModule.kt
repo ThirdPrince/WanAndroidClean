41e6a490861041fe42d7d26c6275a3fd9e77960f
ThirdPrince <18796017665@163.com>
2026-03-01 16:51:47 +0800

    refactor: Remove the presentation module

--- presentation/src/main/java/com/sample/wanandroidclean/presentation/di/PresentationModule.kt
+++
@@ -1,9 +0,0 @@
-package com.sample.wanandroidclean.presentation.di
-
-import com.sample.wanandroidclean.presentation.articles.ArticlesViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val presentationModule = module {
-    viewModel { ArticlesViewModel(get()) }
-}
