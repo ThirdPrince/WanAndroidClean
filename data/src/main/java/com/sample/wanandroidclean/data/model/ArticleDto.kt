b550ff70e557f7802d394c807220b003ccb58d03
ThirdPrince <18796017665@163.com>
2026-03-13 18:25:15 +0800

    - Updated `ArticleDto` to support collection lists by adding an `originId` field.

--- data/src/main/java/com/sample/wanandroidclean/data/model/ArticleDto.kt
+++ data/src/main/java/com/sample/wanandroidclean/data/model/ArticleDto.kt
@@ -1,18 +1,23 @@
 package com.sample.wanandroidclean.data.model
 
 import com.sample.wanandroidclean.domain.entity.Article
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
 
+@InternalSerializationApi /**
+ * Data Transfer Object for articles, compatible with both normal and collection lists.
+ */
 @Serializable
 data class ArticleDto(
-    val id: Int,
-    val title: String,
-    val author: String,
-    val shareUser: String,
-    val link: String
+    val id: Int = 0,
+    val originId: Int = 0, // Used in collection list to refer to the original article id
+    val title: String = "",
+    val author: String = "",
+    val shareUser: String = "",
+    val link: String = ""
 ) {
     fun toDomain(isTop: Boolean = false): Article = Article(
-        id = id,
+        id = if (originId != 0) originId else id,
         title = title,
         author = author,
         shareUser = shareUser,
